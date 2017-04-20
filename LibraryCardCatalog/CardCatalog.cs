ba63fd9d2f5418adee97d2a16bc2a7b2d1e6e9b2
enishiyama <ericryu01@yahoo.com>
2017-04-20 14:14:22 -0500

    change to welcome
    - Deleted some comments from welcome page and turned it into enum.
    -created skeleton for CardCatalogue class

---
+++ LibraryCardCatalog/CardCatalog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCardCatalog
+{
+    public class CardCatalog
+    {
+        private string_filename;
+        private ?? books; //this is the private member variable that contains all of the books
+        public CardCatalog(string fileName) { /*some code*/}
+        public ?? ListBooks(??) { /*code*/}
+        public ?? AddBook(??) { /*code*/}
+        public ?? Save(??) { /*code*/}
+    }
+}
--- LibraryCardCatalog/WelcomePage.cs
+++ LibraryCardCatalog/WelcomePage.cs
@@ -8,53 +8,49 @@ namespace LibraryCardCatalog
 {
     class WelcomePage
     {
-        public static void Intro(string[] args)
+        public static void Intro()
             
         {
             Console.WriteLine("Hello! Please enter the name of a file.");
             Console.ReadLine();
             string filename = Console.ReadLine();
-            Console.WriteLine("Thank you! Your file" + filename + "will be used so you can save your information. Now what can I do for you? To list all books, please press '1'. To add a book, please press '2'. To save and exit, please press '3'");
-
-            //public static void Question05()
-            //{
-            //    Console.WriteLine("Enter a number");
-            //    string input = Console.ReadLine();
-            //    int num1 = Convert.ToInt32(input);
-
-            //    //Option 1:
-            //    string output1;
-            //    if (num1 >= 0)
-            //    {
-            //        output1 = "Number is Positive";
-            //    }
-            //    else //Doesn't need the "if" - there are only two possible outcomes
-            //    {
-            //        output1 = "Number is Negative";
-            //    }
-            //    Console.WriteLine(output);
-
+            Console.WriteLine("Thank you! Your file" + filename + "will be used so you can save your information. Now what can I do for you? To list all books, please press '0'. To add a book, please press '1'. To save and exit, please press '2'");
             string input = Console.ReadLine();
-            int num1 = Convert.ToInt32(input);
+            int choice = int.Parse(input);
+            UserChoice c = (UserChoice)choice;
 
-            string output;
-            if (num1 = 1)
-                
+            switch (c)
             {
-                output = "you have selected list all books";
+                case UserChoice.List:
+                    Console.WriteLine(c.ToString());
+                    break;
+                case Temperature.Add:
+                    Console.WriteLine(c.ToString());
+                    break;
+                case Temperature.Save:
+                    Console.WriteLine(t.ToString());
+                    break;
+                    default:
+                    Console.WriteLine("Invalid Input");
+                    break;
             }
-            else if (num1 = 2)
-            {
-                output = "you have selected add a book " ;
-            }
-            else if (num1 = 3)
-            {
-                output = "your data has been saved! comeback soon...I get lonely.";
-            }
-            else
-            {
-                Console.WriteLine(" Im sorry I cannot process your request. Please input either 1, 2, or 3.");
-            }
+            //string output;
+            //if (num1 = 1)
+            //{
+            //    output = "you have selected list all books";
+            //}
+            //else if (num1 = 2)
+            //{
+            //    output = "you have selected add a book " ;
+            //}
+            //else if (num1 = 3)
+            //{
+            //    output = "your data has been saved! comeback soon...I get lonely.";
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Im sorry I cannot process your request. Please input either 1, 2, or 3.");
+            //}
         }
     }
 }
